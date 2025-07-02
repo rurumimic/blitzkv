d5dd48b4d32239fc8af195066d3053100cd0dee4
rurumimic <10915235+rurumimic@users.noreply.github.com>
2025-07-02 21:40:15 +0900

    hello router

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+
+	r.Run()
 }
