eb6743377327db96155fa8cf9da5a4a55d939f5a
rurumimic <10915235+rurumimic@users.noreply.github.com>
2025-07-03 23:34:16 +0900

    internal handler

---
+++ internal/handler/store.go
@@ -0,0 +1,95 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rurumimic/blitzkv/internal/kvstore"
+)
+
+type Handler struct {
+	store *kvstore.MemStore
+}
+
+func NewStoreHandler(store *kvstore.MemStore) *Handler {
+	return &Handler{
+		store: store,
+	}
+}
+
+func (h *Handler) GetValue(c *gin.Context) {
+	key := c.Query("key")
+
+	if key == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Key is required",
+		})
+		return
+	}
+
+	value, err := h.store.Get(key)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to get value",
+		})
+		return
+	}
+	if value == "" {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Key not found",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"key":   key,
+		"value": value,
+	})
+
+}
+
+func (h *Handler) SetValue(c *gin.Context) {
+	var json struct {
+		Key   string `json:"key" binding:"required"`
+		Value string `json:"value" binding:"required"`
+	}
+
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid input",
+		})
+		return
+	}
+	if err := h.store.Set(json.Key, json.Value); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to set value",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Value set successfully",
+		"key":     json.Key,
+		"value":   json.Value,
+	})
+}
+
+func (h *Handler) DeleteValue(c *gin.Context) {
+	key := c.Query("key")
+	if key == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Key is required",
+		})
+		return
+	}
+	if err := h.store.Delete(key); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to delete key",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Key deleted successfully",
+		"key":     key,
+	})
+
+}
--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rurumimic/blitzkv/internal/handler"
 	"github.com/rurumimic/blitzkv/internal/kvstore"
 )
 
 func main() {
 	store := kvstore.NewMemStore()
 	store.Set("hello", "world")
+
+	storeHandler := handler.NewStoreHandler(store)
 
 	r := gin.Default()
 
@@ -19,78 +22,9 @@ func main() {
 		})
 	})
 
-	r.GET("/store", func(c *gin.Context) {
-		key := c.Query("key")
-		if key == "" {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": "Key is required",
-			})
-			return
-		}
-
-		value, err := store.Get(key)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": "Failed to get value",
-			})
-			return
-		}
-		if value == "" {
-			c.JSON(http.StatusNotFound, gin.H{
-				"error": "Key not found",
-			})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{
-			"key":   key,
-			"value": value,
-		})
-	})
-
-	r.POST("/store", func(c *gin.Context) {
-		var json struct {
-			Key   string `json:"key" binding:"required"`
-			Value string `json:"value" binding:"required"`
-		}
-
-		if err := c.ShouldBindJSON(&json); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": "Invalid input",
-			})
-			return
-		}
-		if err := store.Set(json.Key, json.Value); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": "Failed to set value",
-			})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Value set successfully",
-			"key":     json.Key,
-			"value":   json.Value,
-		})
-	})
-
-	r.DELETE("/store", func(c *gin.Context) {
-		key := c.Query("key")
-		if key == "" {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": "Key is required",
-			})
-			return
-		}
-		if err := store.Delete(key); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": "Failed to delete key",
-			})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Key deleted successfully",
-			"key":     key,
-		})
-	})
+	r.GET("/store", storeHandler.GetValue)
+	r.POST("/store", storeHandler.SetValue)
+	r.DELETE("/store", storeHandler.DeleteValue)
 
 	r.Run()
 }
